0bed4a1ad84ad71cabdb41e421b8b84b626ed25e
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-06 12:11:13 -0500

    arch-independent semihosting

--- arch/hexagon/core/semihost.c
+++ arch/hexagon/core/semihost.c
@@ -1,24 +1,24 @@
+/*
+ * Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/arch/common/semihost.h>
 
 long semihost_exec(enum semihost_instr instr, void *args)
 {
+	register unsigned long r0 __asm__("r0") = instr;
+	register void *r1 __asm__("r1") = args;
+	register long ret __asm__("r0");
 
-    long retval;
-    switch (instr)
-    {
-        case SEMIHOST_WRITEC:
-        {
-            char arg1 = *(char *)args;
-            __asm__ __volatile__ ("r0 = #%1\n\t"
-                                  "r1 = %2\n\t"
-                                  "trap0(#0)\n\t"
-                                  "%0 = r0"
-                                  : "=r"(retval)
-                                  : "i"(SEMIHOST_WRITEC), "r"(&arg1), "m"(arg1)
-                                  : "r0", "r1");
-            return retval;
-        }
-        default:
-            return 0;
-    }
+	__asm__ __volatile__(
+		"r0 = %1\n\t"
+		"r1 = %2\n\t"
+		"trap0(#0)\n\t"
+		"%0 = r0"
+		: "=r"(ret)
+		: "r"(r0), "r"(r1)
+		: "memory");
+
+	return ret;
 }
