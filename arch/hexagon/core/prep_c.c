b491a27c94cfe223ef323bebd44d8e6a435aa816
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-08 22:48:52 -0500

    add qemu support

--- arch/hexagon/core/prep_c.c
+++ arch/hexagon/core/prep_c.c
@@ -10,6 +10,12 @@ extern FUNC_NORETURN void z_cstart(void);
 /* Early fatal error handler */
 void z_hexagon_fatal_error(unsigned int reason)
 {
+	/* Disable interrupts */
+	hexagon_vm_vmsetie(0);
+
+	/* Print error if console is available */
+	printk("FATAL ERROR: Early boot failure, reason=%d\n", reason);
+
 	/* Hang */
 	while (1) {
 		__asm__ volatile("pause(#255)" ::: "memory");
