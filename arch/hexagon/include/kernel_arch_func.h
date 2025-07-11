d9e89f11e56d0f157cefbe19111932d6d70dd371
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-10 23:21:01 -0500

    more progress

--- arch/hexagon/include/kernel_arch_func.h
+++ arch/hexagon/include/kernel_arch_func.h
@@ -9,21 +9,25 @@
 #include <zephyr/kernel_structs.h>
 #include <zephyr/types.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/toolchain.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* Thread context switching */
-extern void arch_switch(void *switch_to, void **switched_from);
-extern unsigned int arch_swap(unsigned int key);
+extern void z_hexagon_arch_switch(void *switch_to, void **switched_from);
+static inline void arch_switch(void *switch_to, void **switched_from)
+{
+	z_hexagon_arch_switch(switch_to, switched_from);
+}
 
 /* Thread creation */
 extern void arch_new_thread(struct k_thread *thread, k_thread_stack_t *stack, char *stack_ptr,
 			    k_thread_entry_t entry, void *p1, void *p2, void *p3);
 
 /* Thread abortion */
-extern void arch_thread_return_value_set(struct k_thread *thread, unsigned int value);
+/* arch_thread_return_value_set is only needed for !CONFIG_USE_SWITCH */
 
 /* Stack pointer manipulation */
 extern char *arch_k_thread_stack_buffer(k_thread_stack_t *stack);
