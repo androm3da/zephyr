41184632455e99161696a739cf311039d06dd3d2
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-10 23:40:55 -0500

    Got some semihosting output
    I was able to boot the system up a bit
    
        $ qemu-system-hexagon -kernel ~/src/toolchain_for_hexagon/hexagonMVM/minivm -device loader,addr=0xc0000000,file=zephyr/zephyr.bin -nographic
        *** Booting Zephyr OS build v4.2.0-rc1-258-gd08c098a3e5e ***
        Hello World! qemu_hexagon/qemu_hexagon_virt

--- arch/hexagon/include/kernel_arch_thread.h
+++ arch/hexagon/include/kernel_arch_thread.h
@@ -1,13 +1,12 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved. */
 
-#ifndef ZEPHYR_ARCH_HEXAGON_INCLUDE_THREAD_H_
-#define ZEPHYR_ARCH_HEXAGON_INCLUDE_THREAD_H_
+#ifndef ZEPHYR_ARCH_HEXAGON_INCLUDE_KERNEL_ARCH_THREAD_H_
+#define ZEPHYR_ARCH_HEXAGON_INCLUDE_KERNEL_ARCH_THREAD_H_
 
 #ifndef _ASMLANGUAGE
-#include <zephyr/types.h>
 
-/* Phase 3: Enhanced thread context structure */
+/* Hexagon thread context structure */
 struct hexagon_thread_context {
 	/* Callee-saved registers */
 	uint32_t r16;
@@ -47,35 +46,9 @@ struct hexagon_thread_context {
 	uint32_t g3;
 };
 
-/* Backward compatibility: old callee_saved structure */
-struct _callee_saved {
-	/* Callee-saved registers (r16-r27) */
-	uint32_t r16;
-	uint32_t r17;
-	uint32_t r18;
-	uint32_t r19;
-	uint32_t r20;
-	uint32_t r21;
-	uint32_t r22;
-	uint32_t r23;
-	uint32_t r24;
-	uint32_t r25;
-	uint32_t r26;
-	uint32_t r27;
-
-	/* Frame pointer and stack pointer */
-	uint32_t r29_fp;
-	uint32_t r30_sp;
-
-	/* Link register */
-	uint32_t r31_lr;
-};
-
-typedef struct _callee_saved _callee_saved_t;
-
-/* Enhanced thread architecture structure */
+/* Extended thread structure */
 struct _thread_arch {
-	/* Enhanced context for Phase 3 */
+	/* Context for thread switching */
 	struct hexagon_thread_context context;
 
 	/* Return value from arch_switch */
@@ -87,12 +60,28 @@ struct _thread_arch {
 	/* Flags */
 	uint8_t flags;
 
-	/* VM event info for compatibility */
-	uint32_t vm_event_info[4]; /* g0-g3 for event handling */
+	/* VM event info for compatibility with existing code */
+	uint32_t vm_event_info[4];
 };
 
-typedef struct _thread_arch _thread_arch_t;
+/* Stack frame for new threads */
+struct init_stack_frame {
+	/* Initial context - matches context switch expectations */
+	struct hexagon_thread_context context;
+
+	/* Space for thread entry parameters */
+	uint32_t entry_point;
+	uint32_t parameter1;
+	uint32_t parameter2;
+	uint32_t parameter3;
+};
 
 #endif /* _ASMLANGUAGE */
 
-#endif /* ZEPHYR_ARCH_HEXAGON_INCLUDE_THREAD_H_ */
+/* Stack alignment requirement */
+#define ARCH_STACK_PTR_ALIGN 8
+
+/* Thread stack size alignment */
+#define ARCH_THREAD_STACK_SIZE_ALIGN 8
+
+#endif /* ZEPHYR_ARCH_HEXAGON_INCLUDE_KERNEL_ARCH_THREAD_H_ */
