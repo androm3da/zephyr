aca3dc0b305306222817220a7cb66bb618530a8f
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-11 23:50:41 -0500

    more board support stuff

--- boards/qemu/hexagon/board.cmake
+++ boards/qemu/hexagon/board.cmake
@@ -2,16 +2,43 @@
 # Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
 
 set(SUPPORTED_EMU_PLATFORMS qemu)
+set(EMU_PLATFORM qemu)
+set(QEMU_ARCH hexagon)
 
 set(QEMU_binary_suffix hexagon)
-set(QEMU_CPU_TYPE_${ARCH} hexagon)
+set(QEMU_CPU_TYPE_${ARCH} v67)
 
+# Basic QEMU flags for Hexagon emulation
 set(QEMU_FLAGS_${ARCH}
+  -cpu ${QEMU_CPU_TYPE_${ARCH}}
   -machine virt
   -nographic
-  -d in_asm
+  -semihosting
+  -m 256M
   )
 
-board_set_debugger_ifnset(qemu)
+# Network support for advanced testing
+if(CONFIG_NET_QEMU_ETHERNET)
+  set(QEMU_NET_ARGS
+    -netdev user,id=net0,hostfwd=tcp::5555-:5555
+    -device virtio-net-device,netdev=net0
+    )
+  list(APPEND QEMU_FLAGS_${ARCH} ${QEMU_NET_ARGS})
+endif()
 
+# Debug support with GDB server
+set(QEMU_DEBUG_FLAGS
+  -gdb tcp::1234
+  -S
+  )
+
+# Set up debugger and flasher
+board_set_debugger_ifnset(qemu)
+board_set_flasher_ifnset(qemu)
+
+# Runner arguments for advanced debugging
 board_runner_args(qemu "--machine;virt")
+board_runner_args(qemu "--gdb-port;1234")
+
+# Finalize runner configuration
+board_finalize_runner_args(qemu)
