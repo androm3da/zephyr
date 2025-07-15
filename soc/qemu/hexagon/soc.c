ba5686e1e2449f2e6c0297621a9050206fa2add7
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-14 22:07:44 -0500

    soc: add support for QEMU hexagon virt machine
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

---
+++ soc/qemu/hexagon/soc.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved. */
+
+#include <zephyr/kernel.h>
+#include <zephyr/arch/hexagon/arch.h>
+
+void soc_early_init_hook(void)
+{
+	/* SoC-specific early initialization */
+}
